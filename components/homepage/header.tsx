ca35f597e105a6c18f6dc1e2acc09a7b1940f20c
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-06-30 09:57:13 +0700

    comment log to prenvent data leak

--- components/homepage/header.tsx
+++ components/homepage/header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
   const favouritesCount = favourites.length;
 
   useEffect(() => {
-    console.log("Client-side API URL:", process.env.NEXT_PUBLIC_API_URL); //check if .env.local is loaded
+    // console.log("Client-side API URL:", process.env.NEXT_PUBLIC_API_URL); //check if .env.local is loaded
   }, []);
 
   useEffect(() => {
